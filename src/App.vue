436fd0024a0d895f0b7657a1ceee79a95aace6d7
Ashish <your.email@example.com>
2025-05-03 22:05:18 +0530

    Adding Props and Slots

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
 import NavBar from "@/components/NavBar.vue";
 import Hero from "./components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
 </script>
 
 <template>
   <NavBar />
   <Hero title="Test title" subtitle="Test subtitle" />
+  <HomeCards />
 </template>
---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps({
+  bg: {
+    type: String,
+    default: "bg-gray-100",
+  },
+});
+</script>
+
+<template>
+  <!-- when we add : to bind the element and whatever we put in double quote is javascript -->
+  <!-- :class is used to bind the class dynamically -->
+  <div :class="`${bg} p-6 rounded-lg shadow-md`">
+    <slot></slot>
+  </div>
+</template>
