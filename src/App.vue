0b116c424ac481cf334434330aeae191bb419d4f
Ashish <your.email@example.com>
2025-05-03 14:37:54 +0530

    Composition API long method example

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <!-- Options API example used in version2 -->
 
 <script lang="ts">
+import { ref } from "vue";
+
 export default {
-  data() {
-    return {
-      name: "John Doe",
-      status: "active",
-      tasks: ["TaskOne", "TaskTwo", "TaskThree"],
-      link: "https://google.com",
-    };
-  },
-  methods: {
-    toggleStatus() {
-      if (this.status === "active") {
-        this.status = "pending";
-      } else if (this.status === "pending") {
-        this.status = "inactive";
+  setup() {
+    // Composition API example used in version3
+    // ref() is a function that creates a reactive reference to a value.
+    // It is used to create reactive data properties in Vue 3.
+    // The ref() function takes an initial value as an argument and returns a reactive reference object.
+    // The value of the reference can be accessed and modified using the .value property.
+
+    const name = ref("John Doe");
+    const status = ref("active");
+    const tasks = ref(["TaskOne", "TaskTwo", "TaskThree"]);
+    const link = ref("https://google.com");
+
+    const toggleStatus = () => {
+      if (status.value === "active") {
+        status.value = "pending";
+      } else if (status.value === "pending") {
+        status.value = "inactive";
       } else {
-        this.status = "active";
+        status.value = "active";
       }
-    },
+    };
+
+    return { name, status, tasks, link, toggleStatus };
   },
 };
 </script>
