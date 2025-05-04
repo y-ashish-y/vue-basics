136a86f0f6ebafad4a9e8bac6e769e2ef95c47ed
Ashish <your.email@example.com>
2025-05-04 20:49:58 +0530

    Implementing Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script lang="ts" setup>
 import NavBar from "@/components/NavBar.vue";
-import Hero from "./components/Hero.vue";
-import HomeCards from "./components/HomeCards.vue";
-import JobListing from "./components/JobListing.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <NavBar />
-  <Hero title="Test title" subtitle="Test subtitle" />
-  <HomeCards />
-  <JobListing :limit="3" :show-button="true" />
+  <RouterView />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListing from "@/components/JobListing.vue";
+</script>
+
+<template>
+  <Hero title="Test title" subtitle="Test subtitle" />
+  <HomeCards />
+  <JobListing :limit="3" :show-button="true" />
+</template>
