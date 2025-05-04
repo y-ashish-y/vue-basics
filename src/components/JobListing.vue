74b54a5b09982e9b20dc4337e4ac0ae667c63341
Ashish <your.email@example.com>
2025-05-04 21:30:36 +0530

    added RouterLink to other pages

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -36,7 +36,7 @@ defineProps<{
   </section>
   <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
     <a
-      href="jobs.html"
+      href="/jobs"
       class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
       >View All Jobs</a
     >
--- src/components/JobListingSingular.vue
+++ src/components/JobListingSingular.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { defineProps, ref, computed } from "vue";
-
+import { RouterLink } from "vue-router";
 interface Job {
   type: string;
   title: string;
@@ -78,12 +78,12 @@ const truncatedDescription = computed(() => {
           <i class="pi pi-map-marker text-orange-700"></i>
           {{ job.location }}
         </div>
-        <a
-          :href="'/job/' + job.id"
+        <RouterLink
+          :to="'/job/' + job.id"
           class="h-[36px] bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
           Read More
-        </a>
+        </RouterLink>
       </div>
     </div>
   </div>
