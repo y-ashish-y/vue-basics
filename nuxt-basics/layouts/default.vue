82550bebdfa0f0f9433c902c8965bd986845777b
Ashish <your.email@example.com>
2025-05-11 10:23:22 +0530

    refactor: restructure app layout and remove unused files

---
+++ nuxt-basics/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nav class="container">
+      <ul>
+        <li><strong>Acme Corp</strong></li>
+      </ul>
+      <ul>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Products</a></li>
+      </ul>
+    </nav>
+    <main class="container">
+      <slot />
+    </main>
+  </div>
+</template>
